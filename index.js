7f7858fa32d33040e22b780d9de8551b04aff9bc
samuelconlon <conlonsp@yahoo.com>
2024-11-18 20:55:59 -0500

    commit tree 2d060dcfd132b7bd89a5c8f182b66867b2501435

--- index.js
+++ index.js
@@ -1,2 +1,62 @@
+const num1 = 31
+const num2 = 2
+const multiply = num1 * num2
 
-// Write your code here
+let random = Math.floor(Math.random() * 10)
+
+const num3 = 16
+const num4 = 6
+
+const mod = num3 % num4
+
+const max = Math.max(1, 3, 4, 20)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const num1 = 31;
+// const num2 = 2;
+// const multiply = num1 * num2;
+
+// let random = Math.floor(Math.random() * 100) + 1;
+
+// const num3 = 15;
+// const num4 = 11;
+// const mod = num3 % num4;
+
+// const max = Math.max(-1, 7, 13, 17, 20);
